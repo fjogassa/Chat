8b8ecb5e3457ca64b84a248b057fa300ee250916
Fabio Jun <fjogassa@bol.com.br>
2017-07-01 10:32:29 -0300

    Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AngularFireDatabase } from 'angularfire2/database';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -6,25 +7,42 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AngularFireModule } from "angularfire2";
+import { ChatPage } from '../pages/chat/chat';
+
+
+export const firebaseConfig = {
+  // Initialize Firebase
+  apiKey: "AIzaSyD-GbMrKwYQWOFOMdwbmZLiwue9JpsCLt4",
+  authDomain: "chat-ionic-a0a18.firebaseapp.com",
+  databaseURL: "https://chat-ionic-a0a18.firebaseio.com",
+  projectId: "chat-ionic-a0a18",
+  storageBucket: "chat-ionic-a0a18.appspot.com",
+  messagingSenderId: "917140940602"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ChatPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ChatPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    AngularFireDatabase,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/pages/chat/chat.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+@IonicPage()
+@Component({
+  selector: 'page-chat',
+  templateUrl: 'chat.html',
+})
+export class ChatPage {
+
+  lista: FirebaseListObservable<any>;
+  mensagem: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public af: AngularFireDatabase) {
+    this.lista=af.list("https://chat-ionic-a0a18.firebaseio.com/chat")
+  }
+
+  enviarMsg() {
+    let msg = {
+      texto: this.mensagem,
+      data: new Date()
+    };
+    
+    this.lista.push(msg).then(()=> {
+      this.mensagem = "";
+      }
+    )
+  }
+
+}
